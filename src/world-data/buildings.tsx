25f1756b8e8133b0d3b9fb1bfb25034a0ded9bff
Fabian Sperber <fabian.sperber@gmx.net>
2021-08-03 23:18:26 +0200

    Remove broken stuff for now

--- src/world-data/buildings.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import { Select } from "../features/utils/ui"
-import { worldData } from "./world-data"
-
-export function SelectBuilding({ value, onChange }: { value: string, onChange?: (id: string) => void }) {
-  return <Select value={value} options={Object.keys(worldData.buildings)} onChange={onChange}></Select>
-}
